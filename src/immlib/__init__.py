5b63c7db2ad1206f63d92bec1ce38d853f16b85f
Noah Benson <n@nben.net>
2025-03-28 17:09:27 -0700

    fill-column cleanup

--- src/immlib/__init__.py
+++ src/immlib/__init__.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
-################################################################################
+###############################################################################
 # immlib/__init__.py
 
 '''``immlib`` is a library of tools for manipulating immutable scientific data.
 
 The ``immlib`` library is designed to enable immutable data structures and lazy
 computation in a scientific context, and it works primarily via a collection of
-utility functions and through the use of decorators, which are generally applied
-to classes and their members to declare how an immutable data-structure's
-members are related.  Taken together, these utilities form a DSL-like system for
-declaring workflows and immutable data-structures with full inheritance support.
+utility functions and through the use of decorators, which are generally
+applied to classes and their members to declare how an immutable
+data-structure's members are related.  Taken together, these utilities form a
+DSL-like system for declaring workflows and immutable data-structures with full
+inheritance support.
 
 Attributes
 ----------
 units : pint.UnitRegistry
-    The registry for units tracked by ``immlib``. The ``immlib.units`` object is
-    a global ``pint``-module unit registry that can be used as a single global
-    place for tracking units. Immlib functions that interact with units
+    The registry for units tracked by ``immlib``. The ``immlib.units`` object
+    is a global ``pint``-module unit registry that can be used as a single
+    global place for tracking units. Immlib functions that interact with units
     generally take an argument ``ureg`` that can be used to modify this
     registry.  Additionally, the default registry (this object,
     ``immlib.units``) can be temporarily changed in a local block using ``with
@@ -34,8 +35,8 @@ submodules : tuple of str
     submodules, including private submodules, are included.
 docproc: docrep.DocstringProcessor object
     This object is used to process all of the doc-strings in the ``immlib``
-    library; it should be used only with the ``immlib.docwrap`` decorator, which
-    can safely be applied anywhere in a sequence of decorators and which
+    library; it should be used only with the ``immlib.docwrap`` decorator,
+    which can safely be applied anywhere in a sequence of decorators and which
     correctly applies the ``wraps`` decorator to its argument. Function
     documentation is always processed using the ``sections=('Parameters',
     'Returns', 'Raises', 'Examples', 'Inputs', 'Outputs')`` parameter and the
@@ -44,7 +45,7 @@ docproc: docrep.DocstringProcessor object
 '''
 
 
-# Imports ######################################################################
+# Imports #####################################################################
 
 # We always load _init first.
 from ._init    import reclaim
@@ -56,8 +57,9 @@ from .iolib    import *
 from .workflow import *
 from .types    import *
 # Import the Global UnitRegistry object to the global immlib scope. This is the
-# value that gets updated when one runs `immlib.default_ureg()`, and this is the
-# UnitRegistry that is used as the default registry for all ``immlib`` functions.
+# value that gets updated when one runs `immlib.default_ureg()`, and this is
+# the UnitRegistry that is used as the default registry for all ``immlib``
+# functions.
 from .util._quantity import _initial_global_ureg as units
 # Do the same for the global DocstringProcessor (from the docrep library) from
 # the doc subpackage.
@@ -66,7 +68,7 @@ from .doc._core import _initial_global_docproc as docproc
 from ._version import (version, Version)
 
 
-# Modules/Reloading ############################################################
+# Modules/Reloading ###########################################################
 
 submodules = (
     'immlib._init',
@@ -91,8 +93,8 @@ submodules = (
 def reload_immlib():
     """Reload and return the entire ``immlib`` package.
 
-    ``immlib.reload_immlib()`` reloads every submodule in the ``immlib`` package
-    then reloads ``immlib`` itself, and returns the reloaded package.
+    ``immlib.reload_immlib()`` reloads every submodule in the ``immlib``
+    package then reloads ``immlib`` itself, and returns the reloaded package.
 
     .. Warning:: This function exists primarily for debugging purposes; its use
         is not generally needed or advised by users of the library.
@@ -116,7 +118,7 @@ def reload_immlib():
     return importlib.reload(sys.modules[__name__])
 
 
-# Package Meta-Code ############################################################
+# Package Meta-Code ###########################################################
 
 __version__ = version.string
 __all__ = tuple(
--- src/immlib/_init.py
+++ src/immlib/_init.py
@@ -1,39 +1,39 @@
 # -*- coding: utf-8 -*-
-################################################################################
+###############################################################################
 # immlib/_init.py
 
 """Utilities and global definitions needed by theoretically the entire rest of
 the `immlib` library during the import process.
 
-The `immlib._init` module is the first submodule of the `immlib` library that is
-loaded during the import process and is the firt that is reloaded when the
-`immlib.reload_immlib()` library is called. Accordingly, functions and variables
-that are needed by other modules can be put here. These functions and variables,
-if they are intended to be public, should be reclaimed by another module (see
-the `immlib._init.reclaim` / `immlib.util.reclaim` function).
+The `immlib._init` module is the first submodule of the `immlib` library that
+is loaded during the import process and is the firt that is reloaded when the
+`immlib.reload_immlib()` library is called. Accordingly, functions and
+variables that are needed by other modules can be put here. These functions and
+variables, if they are intended to be public, should be reclaimed by another
+module (see the `immlib._init.reclaim` / `immlib.util.reclaim` function).
 """
 
 
-# Dependencies #################################################################
+# Dependencies ################################################################
 
 import sys
 from types import ModuleType
 
 
-# Utility Functions ############################################################
+# Utility Functions ###########################################################
 
 def reclaim(modname, attributes=Ellipsis, *,
             del_reclaim=False,
             skip_externs=True,
             skip_private=True,
             skip_modules=True):
-    """Sets the ``__module__`` attributes of objects in the module whose name is
-    `modname` to be `modname`.
+    """Sets the ``__module__`` attributes of objects in the module whose name
+    is `modname` to be `modname`.
 
     ``reclaim(__name__, __all__)``, when run at the end of an ``__init__.py``
     file, will set the module names of all objects in that module's ``__all__``
-    list to be ``__name__``, effectively hiding any submodule ownership of those
-    objects.
+    list to be ``__name__``, effectively hiding any submodule ownership of
+    those objects.
 
     ``reclaim(__name__)`` attempts to reclaim all attributes in the module, not
     just those in the ``__all__`` list.
@@ -41,8 +41,8 @@ def reclaim(modname, attributes=Ellipsis, *,
     Parameters
     ----------
     modname : str
-        The name of the module to reclaim. Typically, ``reclaim`` is called from
-        an ``__init__.py`` file using the syntax ``reclaim(__name__)`` or
+        The name of the module to reclaim. Typically, ``reclaim`` is called
+        from an ``__init__.py`` file using the syntax ``reclaim(__name__)`` or
         ``reclaim(__name__, __all__)``.
     attributes : Ellipsis | iterable of str, optional
         The attributes to reclaim. If this is explicitly given then it must be
@@ -65,19 +65,19 @@ def reclaim(modname, attributes=Ellipsis, *,
         which ``reclaim`` is run are only reclaimed if they belong to a
         submodule of of that module.
 
-        .. warning:: If `skip_externs` is set to ``False``, attributes like
+        .. Warning:: If `skip_externs` is set to ``False``, attributes like
             ``np`` from the import statement ``import numpy as np`` at the top
             of an ``__init__.py`` file can result in the ``numpy`` module being
-            reclaimed (if ``skip_modules`` is also ``False``). Similarly, if the
-            line ``from immlib.util import reclaim`` appears in an
+            reclaimed (if ``skip_modules`` is also ``False``). Similarly, if
+            the line ``from immlib.util import reclaim`` appears in an
             ``__init__.py`` file followed by ``reclaim(__name__,
             skip_externs=False)`` then the ``reclaim`` function will be
             reclaimed by the module.
     skip_private : boolean, optional
-        The `skip_private` option may be set to ``False`` to instruct the function
-        to claim ownership over attributes whose names begin with the underscore
-        (``_``) charcter. If this is not provided or is ``True``, then such
-        attributes are skipped.
+        The `skip_private` option may be set to ``False`` to instruct the
+        function to claim ownership over attributes whose names begin with the
+        underscore (``_``) charcter. If this is not provided or is ``True``,
+        then such attributes are skipped.
     skip_modules : boolean, optional
         The `skip_modules` option (default: ``True``) may be set to false to
         instruct the ``reclaim`` function to attempt to reclaim modules as well
@@ -87,7 +87,6 @@ def reclaim(modname, attributes=Ellipsis, *,
     -------
     str
         The module named by the given `modname` argument.
-
     """
     mod = sys.modules[modname]
     for k in dir(mod):
